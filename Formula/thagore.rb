6846fee63084f452060d394ef22a5ebe5621ce1e
Lê Hùng Quang Minh <lhquangmink@gmail.com>
2026-02-13 20:09:10 +0700

    chore: initialize thagore formula

---
+++ Formula/thagore.rb
@@ -0,0 +1,17 @@
+﻿class Thagore < Formula
+  desc "Thagore self-hosted compiler"
+  homepage "https://github.com/thagore-foundation/thagore"
+  url "https://github.com/thagore-foundation/thagore/releases/download/v0.0.0/thag-linux.tar.gz"
+  sha256 "REPLACE_ME"
+  license "MIT"
+  version "0.0.0"
+
+  def install
+    bin.install "dist/bin/thag"
+    libexec.install "dist/lib/std"
+  end
+
+  test do
+    assert_match "Thagore", shell_output("#{bin}/thag --help 2>&1", 0)
+  end
+end
