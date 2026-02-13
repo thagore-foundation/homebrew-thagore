e5330bfce23dc0f71a75391b0c5ad1cbd1ff980e
Lê Hùng Quang Minh <158133523+lehungquangminh@users.noreply.github.com>
2026-02-13 20:12:20 +0700

    Update thagore to 0.3.13-foundation

--- Formula/thagore.rb
+++ Formula/thagore.rb
@@ -1,10 +1,10 @@
 ﻿class Thagore < Formula
   desc "Thagore self-hosted compiler"
   homepage "https://github.com/thagore-foundation/thagore"
-  url "https://github.com/thagore-foundation/thagore/releases/download/v0.0.0/thag-linux.tar.gz"
-  sha256 "REPLACE_ME"
+  url "https://github.com/thagore-foundation/thagore/releases/download/v0.3.13-foundation/thag-linux.tar.gz"
+  sha256 "37f0e0ec2d39d4e6069420e0db4abcfee5645da8002f81c2e0db4b7bcd689f66"
   license "MIT"
-  version "0.0.0"
+  version "0.3.13-foundation"
 
   def install
     bin.install "dist/bin/thag"
